18a08e75f4b9551d8c353011ab4aee2d5478c5e7
sahil3554 <sahiltagra3554@gmail.com>
2026-03-04 20:22:40 +0530

    fixed routing

---
+++ internal/routes/v1/routes.go
@@ -0,0 +1,16 @@
+package v1
+
+import (
+	"github.com/gin-gonic/gin"
+	// "qr_url_service/internal/handlers"
+)
+
+func RegisterRoutes(rg *gin.RouterGroup) {
+
+	
+	// url := rg.Group("/url")
+	{
+		// url.POST("/shorten", handlers.CreateShortURL)
+		// url.GET("/:code", handlers.RedirectURL)
+	}
+}
